ee91b2685cb52c0c3e721d425df7fe6f8608672d
palmcivet7 <palmcivet@proton.me>
2023-11-04 19:11:05 +0000

    import Client.sol and fix _ccipReceive visability

--- src/CCIDReceiver.sol
+++ src/CCIDReceiver.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 
 contract CCIDReceiver is CCIPReceiver {
     event KYCStatusReceived(string status);
 
     constructor(address _router) CCIPReceiver(_router) {}
 
-    function _ccipReceive(Client.Any2EVMMessage memory message) external override {
+    function _ccipReceive(Client.Any2EVMMessage memory message) internal override {
         string memory status = abi.decode(message.data, (string));
         emit KYCStatusReceived(status);
     }
