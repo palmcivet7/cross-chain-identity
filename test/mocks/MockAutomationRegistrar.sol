ca38994a9dc07e7cd56336e760ce886e54c89507
palmcivet7 <palmcivet@proton.me>
2024-05-12 08:05:28 +0100

    initialize mock upkeepId at 1

--- test/mocks/MockAutomationRegistrar.sol
+++ test/mocks/MockAutomationRegistrar.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.19;
 import {IAutomationRegistrar, RegistrationParams} from "../../src/v2/interfaces/IAutomationRegistrar.sol";
 
 contract MockAutomationRegistrar is IAutomationRegistrar {
-    uint256 private upkeepId;
+    uint256 private upkeepId = 1;
 
     function registerUpkeep(RegistrationParams calldata) external returns (uint256) {
         return upkeepId++;
